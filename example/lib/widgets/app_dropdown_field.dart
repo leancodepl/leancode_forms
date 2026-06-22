274792fe09a62957ba339218a7f9c672c60e2a19
aydinguven-leancode <aydin.guven@leancode.pl>
2026-06-22 15:21:36 +0200

    Replaced .map with for loop

--- example/lib/widgets/app_dropdown_field.dart
+++ example/lib/widgets/app_dropdown_field.dart
@@ -32,14 +32,13 @@ class AppDropdownField<T> extends StatelessWidget {
           child: DropdownButtonFormField<T>(
             value: value,
             onChanged: onChanged,
-            items: options
-                .map(
-                  (e) => DropdownMenuItem(
-                    value: e,
-                    child: Text(labelBuilder(e)),
-                  ),
-                )
-                .toList(),
+            items: [
+              for (final option in options)
+                DropdownMenuItem(
+                  value: option,
+                  child: Text(labelBuilder(option)),
+                ),
+            ],
             decoration: InputDecoration(
               labelText: label,
               hintText: hint,
